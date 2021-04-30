798a4a6b947264ad7a7b19ac4cb265f9a8b49f20
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-29 22:20:02 -0400

    form working with login

--- pages/form.js
+++ pages/form.js
@@ -1,72 +1,65 @@
-import { useState } from 'react';
+import useForm from '../src/Form/useForm';
 
 const Form = () => {
-  //const [input, setInput] = useState('');
-  const [info, setInfo] = useState({
-    name: '',
-    password: '',
-    email: ''
-  });
-
-  const handleChange = e => {
-    const { name, value } = e.target;
-
-    setInfo({ ...info, [name]: value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-
-    if (info.name.length <= 3) {
-      console.log('Name should be atleast 3 characters long');
-    }
-
-    if (!info.email.toLowerCase().includes('@')) {
-      console.log('enter valid email');
-    }
-
-    if (info.password.length <= 6) {
-      console.log('Name should be atleast 6 characters long');
-    }
-
-  };
-
-  console.log(info);
+  const {
+    handleChange,
+    handleSubmit,
+    info,
+    nameError,
+    emailError,
+    passwordError,
+    successLogin
+  } = useForm();
 
   return (
     <>
-      <section className="flex items-center h-screen">
-        <form
-          className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            value={info.name}
-            name="name"
-            placeholder="Enter your name"
-            onChange={handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            value={info.email}
-            name="email"
-            placeholder="Enter your email"
-            onChange={handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            value={info.password}
-            name="password"
-            placeholder="Enter your password"
-            onChange={handleChange}
-          />
-          <button>Submit</button>
-        </form>
-      </section>
+      {!successLogin ? (
+        <section className="flex items-center h-screen">
+          <form
+            className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              value={info.name}
+              name="name"
+              placeholder="Enter your name"
+              onChange={handleChange}
+            />
+            {nameError && <div className="text-red-700">{nameError}</div>}
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              value={info.email}
+              name="email"
+              placeholder="Enter your email"
+              onChange={handleChange}
+            />
+            {emailError && <div className="text-red-700">{emailError}</div>}
+
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              value={info.password}
+              name="password"
+              placeholder="Enter your password"
+              onChange={handleChange}
+            />
+            {passwordError && (
+              <div className="text-red-700">{passwordError}</div>
+            )}
+
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+      ) : (
+        <section className="flex items-center h-screen">
+          <div
+            className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5"
+          >LOGIN SUCCESS</div>
+        </section>
+      )}
     </>
   );
 };
