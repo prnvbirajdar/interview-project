041a1ca552f2347d093997214d466669c7623880
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-30 03:42:39 -0400

    dropdown fully working

--- pages/dropdown.js
+++ pages/dropdown.js
@@ -2,29 +2,52 @@ import React, { useState } from 'react';
 import data from '../src/DropdownComponents/data';
 
 export default function dropdown() {
-  const [filteredList, setFilteredList] = useState([]);
-
-  const [courseOptions, setCourseOptions] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState('');
 
   let coursesTags = data.map(({ tag }) => tag);
   coursesTags = [...new Set(coursesTags)];
 
-  coursesTags.map(tag => console.log(tag));
+  const newList = data.filter(({ tag }) => tag === selectedTopic);
+
+  console.log(newList);
   return (
     <>
       <section className="flex h-screen">
         <div className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5">
           <form action="/action_page.php">
             <label htmlFor="courses">Choose a topic:</label>
-            <select name="topic">
+            <select
+              className="border bg-gray-100 w-full p-2 rounded-md"
+              name="topic"
+              onChange={e => setSelectedTopic(e.target.value)}
+            >
+              <option value="All">All</option>
               {coursesTags?.map((tag, index) => (
                 <option key={index} value={tag}>
                   {tag}
                 </option>
               ))}
             </select>
-            {/* <input type="submit" value="Submit" /> */}
           </form>
+          <ul>
+            {selectedTopic === '' || selectedTopic === 'All'
+              ? data.map(({ id, course }) => (
+                  <li
+                    className="m-2 p-2 border bg-indigo-200 text-bold rounded-lg text-center"
+                    key={id}
+                  >
+                    {course}
+                  </li>
+                ))
+              : newList.map(({ id, course }) => (
+                  <li
+                    className="m-2 p-2 border bg-indigo-200 text-bold rounded-lg text-center"
+                    key={id}
+                  >
+                    {course}
+                  </li>
+                ))}
+          </ul>
         </div>
       </section>
     </>
