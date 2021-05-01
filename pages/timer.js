99fb1c408edbce324f5e2e6d66c20f91a5229df6
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-01 17:36:57 -0400

    timer working with min and sec and hrs

--- pages/timer.js
+++ pages/timer.js
@@ -1,30 +1,37 @@
 import React from 'react';
 
 export default function timer() {
-  const [time, setTime] = React.useState(0);
+  const [time, setTime] = React.useState(3661);
   const [timerOn, setTimerOn] = React.useState(false);
 
-  React.useEffect(() => {
-    if (timerOn) {
-      const interval = setInterval(() => {
-        setTime(time => time + 1);
-      }, 1000);
+    React.useEffect(() => {
+      if (timerOn) {
+        const interval = setInterval(() => {
+          setTime(time => time + 1);
+        }, 1000);
 
-      return () => clearTimeout(interval);
-    } else {
-      setTime(0);
-    }
-  }, [timerOn]);
+        return () => clearTimeout(interval);
+      } else {
+        setTime(0);
+      }
+    }, [timerOn]);
 
   return (
     <>
       <section className="flex items-center h-screen">
         <div className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5 ">
           <div className="text-center m-5 space-y-2">
-            <p>Stopwatch: </p>{' '}
-            <p>
-              {Math.floor(time / 60)} min : {time % 60} sec
-            </p>
+            <p>Stopwatch: </p>
+            {time < 3600 ? (
+              <p>
+                {Math.floor(time / 60)} min : {time % 60} sec
+              </p>
+            ) : (
+              <p>
+                {Math.floor(time / 3600)} hr : {Math.floor((time % 3600) / 60)}{' '}
+                min : {time % 60} sec
+              </p>
+            )}
           </div>
           <div className="mx-auto space-x-5">
             {!timerOn ? (
@@ -38,12 +45,12 @@ export default function timer() {
               </button>
             ) : (
               <div className="space-x-5">
-                 <button
+                <button
                   className="py-2 px-4 mx-auto bg-blue-400 rounded-lg"
-                //   onClick={handleStop}
+                  //   onClick={handleStop}
                 >
                   Pause
-                </button> 
+                </button>
                 <button
                   className="py-2 px-4 mx-auto bg-red-400 rounded-lg"
                   onClick={() => {
