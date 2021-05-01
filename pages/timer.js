004d020e419741c504a33fbb7aa8f37cf27ab9fe
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-01 17:32:18 -0400

    timer working with min and sec

--- pages/timer.js
+++ pages/timer.js
@@ -16,35 +16,44 @@ export default function timer() {
     }
   }, [timerOn]);
 
-  const handleStart = () => {
-    setTimerOn(true);
-  };
-
-  const handleStop = () => {
-    setTimerOn(false);
-  };
-
-  console.log(timerOn);
   return (
     <>
       <section className="flex items-center h-screen">
         <div className="container flex flex-col mx-auto w-2/3 border p-6 space-y-5 ">
           <div className="text-center m-5 space-y-2">
-            <p>Stopwatch: </p> <p>{time}</p>
+            <p>Stopwatch: </p>{' '}
+            <p>
+              {Math.floor(time / 60)} min : {time % 60} sec
+            </p>
           </div>
           <div className="mx-auto space-x-5">
-            <button
-              className="py-2 px-4 mx-auto bg-green-400 rounded-lg"
-              onClick={handleStart}
-            >
-              Start
-            </button>
-            <button
-              className="py-2 px-4 mx-auto bg-red-400 rounded-lg"
-              onClick={handleStop}
-            >
-              Stop
-            </button>
+            {!timerOn ? (
+              <button
+                className="py-2 px-4 mx-auto bg-green-400 rounded-lg"
+                onClick={() => {
+                  setTimerOn(true);
+                }}
+              >
+                Start
+              </button>
+            ) : (
+              <div className="space-x-5">
+                 <button
+                  className="py-2 px-4 mx-auto bg-blue-400 rounded-lg"
+                //   onClick={handleStop}
+                >
+                  Pause
+                </button> 
+                <button
+                  className="py-2 px-4 mx-auto bg-red-400 rounded-lg"
+                  onClick={() => {
+                    setTimerOn(false);
+                  }}
+                >
+                  Stop
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </section>
