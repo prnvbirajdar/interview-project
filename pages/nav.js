7491deef248c7e76d7501245eeb11a1919feef44
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-01 22:33:58 -0400

    nav

---
+++ pages/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function nav() {
+  return (
+    <>
+      <header>
+        <p>Logo</p>
+
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>About</li>
+        </ul>
+      </header>
+    </>
+  );
+}
