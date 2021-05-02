274885eab01241406e47b6065ff093292ad22661
prnvbirajdar <prnvbirajdar@gmail.com>
2021-05-02 01:37:22 -0400

    nav responsive

--- pages/nav.js
+++ pages/nav.js
@@ -1,18 +1,58 @@
 import React from 'react';
+import Link from 'next/link';
+import Sidebar from '../src/Sidebar/sidebar';
+
+const navOptions = ['Home', 'Projects', 'Blog', 'About'];
 
 export default function nav() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <>
-      <header>
-        <p>Logo</p>
+      <header className="flex justify-between items-center w-full border px-10 h-12 bg-blue-300">
+        <div className="z-40">Logo</div>
 
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>Blog</li>
-          <li>About</li>
-        </ul>
+        <nav className="md:block hidden">
+          <ul className="flex space-x-10 ">
+            {navOptions.map((option, index) => {
+              return (
+                <li key={index}>
+                  <Link href="/">
+                    <a>{option}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        {!isOpen && (
+          <div className=" block md:hidden" onClick={() => setIsOpen(true)}>
+            <Menu />
+          </div>
+        )}
       </header>
+
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} navOptions={navOptions} />
     </>
   );
 }
+
+const Menu = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 cursor-pointer"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  );
+};
